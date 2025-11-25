2213e15a49746b7f15efc5ebeab6ff5451e666f2
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-11-25 17:43:54 +0300

    path modules

--- 9-path-module.js
+++ 9-path-module.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 console.log(path.sep);
 
-const filePath = path.join('\content','subfolder','test.txt');
+const filePath = path.join('\content', 'subfolder', 'test.txt');
 
 console.log(filePath);
 
@@ -10,6 +10,6 @@ const base = path.basename(filePath);
 
 console.log(base);
 
-const absolute = path.resolve(__dirname,'content','subfolder','test.txt');
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
 
 console.log(absolute);
